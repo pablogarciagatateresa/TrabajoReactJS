61c40b72771bc6f43c04bfb829a83e873b6571b9
pablogarciagatateresa <106460893+pablogarciagatateresa@users.noreply.github.com>
2022-06-19 21:29:43 -0500

    Add files via upload

---
+++ App Mongo/server.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv').config();
+const app = require('./app');
+const {  mongoConn } = require('./databases/configuration');
+
+
+// configuraciones
+app.set('port', process.env.PORT || 3000);
+
+const conn = mongoConn()
+// escucha
+app.listen(app.get('port'), () => {
+    console.log(`Arranca el servidor en el puerto: ${app.get('port')}`)
+});
